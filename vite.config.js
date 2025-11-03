7e8e4af70b06e16051cb86af87edc4431d238cda
BekzhanMost <beckzhan111@mail.ru>
2025-11-03 12:19:21 +0500

    third commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ВАЖНО: для https://bekzhanmost.github.io базовый путь — корень
+  base: '/BekzhanMost/', // имя репозитория, с тем же регистром
 })
